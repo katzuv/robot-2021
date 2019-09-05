3277d678ba229a172def11310e35b2a4071a5156
LIor <lior5858@gmail.com>
2019-09-05 19:05:41 +0300

    Create a command for setting speed for the drivetrain

---
+++ src/main/java/robot/subsystems/Commands/DriveCommand.java
@@ -0,0 +1,39 @@
+package robot.subsystems.Commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+
+public class DriveCommand extends Command {
+
+    private double speed;
+
+    public DriveCommand(double speed){
+        this.speed = speed;
+    }
+
+    @Override
+    protected void initialize() {
+        Robot.drivetrain.setLeftSpeed(speed);
+        Robot.drivetrain.setRightSpeed(speed);
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+
+    @Override
+    protected void end() {
+
+    }
+}
