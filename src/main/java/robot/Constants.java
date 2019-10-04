492d1d2b10dfc73d1bbe3f2c0d429e83bcd01a52
Paulo Khayat <paulokhayat@gmail.com>
2019-10-04 09:44:06 +0300

    Add documentation

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -1,5 +1,10 @@
 package robot;
 
+/**
+ * A class holding all of the constants of every mechanism on the robot.
+ * Place global constants in this class, and mechanism-specific constants inside their respective mechanism subclass.
+ * When accessing a mechanism-specific port, call Constants.[MECHANISM].[CONSTANT]
+ */
 public class Constants {
     //All general constants go here
     //public static final double TIME_STEP = 0.02;
