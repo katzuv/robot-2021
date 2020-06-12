f9919b6dc0b11f56e0960ac9026ba50b8d4526f9
Adam <adam.vichik@gmail.com>
2020-06-12 16:59:55 +0300

    Fix conversion to Polar

--- src/main/java/robot/Utils.java
+++ src/main/java/robot/Utils.java
@@ -20,7 +20,7 @@ public class Utils {
     public static double[] cartesianToPolar(double x, double y) {
         double[] polar = new double[2];
         polar[0] = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-        polar[1] = Math.toDegrees(Math.acos(y/polar[0]));
+        polar[1] = Math.toDegrees(Math.atan2(y, polar[0]));
 
         return polar;
     }
