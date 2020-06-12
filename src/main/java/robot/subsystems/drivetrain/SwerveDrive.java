6e0823bf9a3e7bcd596d404c1edc6dedbbbf425f
Adam <adam.vichik@gmail.com>
2020-06-12 11:10:51 +0300

    Add forward, strafe and angle parameters to SwerveDrive

--- src/main/java/robot/subsystems/drivetrain/SwerveDrive.java
+++ src/main/java/robot/subsystems/drivetrain/SwerveDrive.java
@@ -8,13 +8,21 @@ public class SwerveDrive {
     private final SwerveModule wheelFrontLeft;
     private final SwerveModule wheelBackRight;
     private final SwerveModule wheelBackLeft;
+    private double forward;
+    private double strafe;
+    private double angle;
 
-    public SwerveDrive() {
+    public SwerveDrive(double forward, double strafe, double angle) {
         wheelFrontRight = new SwerveModule(new TalonSRX(10), new TalonSRX(10));
         wheelFrontLeft = new SwerveModule(new TalonSRX(10), new TalonSRX(10));
         wheelBackRight = new SwerveModule(new TalonSRX(10), new TalonSRX(10));
         wheelBackLeft = new SwerveModule(new TalonSRX(10), new TalonSRX(10));
+
+        this.forward = forward;
+        this.strafe = strafe;
+        this.angle = angle;
     }
+
 
 
 
