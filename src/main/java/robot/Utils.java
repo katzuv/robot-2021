e8ad64abb1e198b2cf3efa47eaab3d50f524f553
Adam <adam.vichik@gmail.com>
2020-06-12 13:30:28 +0300

    Add Utils file

---
+++ src/main/java/robot/Utils.java
@@ -0,0 +1,20 @@
+package robot;
+
+public class Utils {
+
+    /**
+     * recreates the results of Math.floorMod() for Double type variables.
+     * The result is the unsigned remainder of the mod method.
+     *
+     * @param value the numerator
+     * @param mod   the denominator
+     * @return the remainder of the division
+     */
+    public static double floorMod(double value, double mod) {
+        value %= mod;
+        value += mod;
+        value %= mod;
+        return value;
+    }
+    
+}
