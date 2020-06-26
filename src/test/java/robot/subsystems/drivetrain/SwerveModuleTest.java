5688bb4e5e9eb00287366b256a45a852c3c226d6
Adam <adam.vichik@gmail.com>
2020-06-26 21:44:45 +0300

    Add test code to method

--- src/test/java/robot/subsystems/drivetrain/SwerveModuleTest.java
+++ src/test/java/robot/subsystems/drivetrain/SwerveModuleTest.java
@@ -1,6 +1,7 @@
 package robot.subsystems.drivetrain;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,6 +18,9 @@ public class SwerveModuleTest {
 
     @Test
     public void setTargetAngle() {
+        double targetAngle = swerveModule.setTargetAngle(-1.5 * Math.PI, -Math.PI / 4);
+        double expectedAngle = Math.PI / 2;
 
+        Assert.assertEquals(expectedAngle, targetAngle, 0.01);
     }
 }
