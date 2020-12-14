62ea609bd1d59b2b1f9a4fc051d2abd099996302
natymeitav <naty.meitav@Gmail.com>
2020-12-14 13:47:43 +0200

    Add setPower to Intake.

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Constants;
@@ -10,11 +11,18 @@ public class Intake {
     private TalonSRX motor = new TalonSRX(Ports.Intake.MOTOR);
     private Solenoid solenoidR = new Solenoid(Ports.Intake.SOLENOID_RIGHT);
     private Solenoid solenoidL = new Solenoid(Ports.Intake.SOLENOID_LEFT);
+    private UnitModel unitModel;
 
     public Intake() {
         motor.setInverted(Ports.Intake.IS_INVERTED);
-        UnitModel unitModel = new UnitModel(Constants.Intake.TICK_PER_METER);
+        unitModel = new UnitModel(Constants.Intake.TICK_PER_METER);
+    }
+
     public double getVelocity(){
         return unitModel.toVelocity(motor.getSelectedSensorVelocity());
+    }
+
+    public void setPower(double power){
+        motor.set(ControlMode.PercentOutput,power);
     }
 }
