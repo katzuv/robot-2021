81dddd224cc1d183556da78dd6626b63deace0fc
saar <saarzi2004@gmail.com>
2020-12-14 17:02:43 +0200

    Fix Pull.

--- src/main/java/frc/robot/subsystems/climber/commands/Pull.java
+++ src/main/java/frc/robot/subsystems/climber/commands/Pull.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.climber.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+import frc.robot.Constants;
 import frc.robot.subsystems.climber.Climber;
 
 public class Pull extends CommandBase {
@@ -11,18 +12,27 @@ public class Pull extends CommandBase {
 
     public Pull(Climber climber, double height) {
         this.climber = climber;
-        addRequirements(climber);
         this.height = height;
+        addRequirements(climber);
     }
 
     @Override
     public void initialize() {
+        if (climber.getStopperMode() == Climber.PistonMode.OPEN)
+            new SwitchMechanicalStopper(climber);
+        if (climber.getGearboxMode() == Climber.PistonMode.CLOSED)
+            new SwitchGearbox(climber);
         climber.setHeight(height);
     }
 
     @Override
     public void execute() {
 
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Math.abs(climber.getHeight() - height) <= Constants.Climber.TOLERANCE;
     }
 
     @Override
