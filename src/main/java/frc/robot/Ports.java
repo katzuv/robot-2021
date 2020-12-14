04c1f53d8073a69082b71e89f177ce0f8cdfbd44
saar <saarzi2004@gmail.com>
2020-12-14 16:36:15 +0200

    Add Ports.

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Ports {
+
+    public static class ColourWheel {
+        public static final int MOTOR = 0;
+        public static final int COLOR_SENSOR = 0;
+    }
+}
