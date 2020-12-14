2a15b34c6f1819926808a0fd792db0ba745564c7
saar <saarzi2004@gmail.com>
2020-12-14 17:24:53 +0200

    Create Ports.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,6 +4,10 @@ public class Ports {
 
     public static class ColourWheel {
         public static final int MOTOR = 0;
-        public static final int COLOR_SENSOR = 0;
+
+        public static final boolean MOTOR_INVERTED = false;
+
+        public static final boolean MOTOR_SENSOR_PHASE_INVERTED = false;
+
     }
 }
