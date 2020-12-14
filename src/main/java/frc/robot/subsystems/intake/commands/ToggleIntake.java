1294889f6ae834ff25bddf4895bd59597a14036f
natymeitav <naty.meitav@Gmail.com>
2020-12-14 17:06:08 +0200

    Document ToggleIntake.

--- src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
+++ src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
@@ -4,6 +4,10 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.intake.Intake;
 
 public class ToggleIntake extends InstantCommand {
+    /**
+     * this command toggles the state of intake's pistons
+     * i.e. opened --> closed & closed --> opened
+     */
 
     private Intake intake;
     public ToggleIntake(Intake i) {
