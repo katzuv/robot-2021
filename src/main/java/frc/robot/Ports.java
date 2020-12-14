c12e0c15d0b92720318e4112c46afbeb465cacff
saar <saarzi2004@gmail.com>
2020-12-14 11:21:47 +0200

    Create Ports class.

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+public class Ports {
+
+    public static class Climber {
+        public static final int LEFT_MOTOR = 0;
+
+        public static final int RIGHT_MOTOR = 0;
+
+        public static final boolean LEFT_MOTOR_INVERTED = false;
+
+        public static final boolean LEFT_SENSOR_PHASE_INVERTED = false;
+
+        public static final boolean RIGHT_MOTOR_INVERTED = false;
+
+        public static final boolean RIGHT_SENSOR_PHASE_INVERTED = false;
+    }
+}
