a7fdcb62a9207c0b18f3a8547a19d411a6cfe7b0
natymeitav <naty.meitav@Gmail.com>
2020-12-14 11:08:33 +0200

    Add Intake's ports.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,5 +2,9 @@ package frc.robot;
 
 public class Ports {
     public static final class Intake{
+        public static final int MOTOR = 0;
+        public static final boolean IS_INVERTED = false;
+        public static final int SOLENOID_RIGHT = 1;
+        public static final int SOLENOID_LEFT = 2;
     }
 }
