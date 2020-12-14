3717a0339979bb1fcbfa8f5ccf8a4088dbac9fc0
natymeitav <naty.meitav@Gmail.com>
2020-12-14 13:50:20 +0200

    Add getters to piston's state.

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -3,23 +3,31 @@ package frc.robot.subsystems.intake;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Ports;
 import frc.robot.subsystems.UnitModel;
 
-public class Intake {
+public class Intake extends SubsystemBase {
     private TalonSRX motor = new TalonSRX(Ports.Intake.MOTOR);
     private Solenoid solenoidR = new Solenoid(Ports.Intake.SOLENOID_RIGHT);
     private Solenoid solenoidL = new Solenoid(Ports.Intake.SOLENOID_LEFT);
-    private UnitModel unitModel;
+    private UnitModel unitModel = new UnitModel(Constants.Intake.TICK_PER_METER);
 
     public Intake() {
         motor.setInverted(Ports.Intake.IS_INVERTED);
-        unitModel = new UnitModel(Constants.Intake.TICK_PER_METER);
     }
 
     public double getVelocity(){
         return unitModel.toVelocity(motor.getSelectedSensorVelocity());
+    }
+
+    public boolean getStateRight(){
+        return solenoidR.get();
+    }
+
+    public boolean getStateL(){
+        return solenoidL.get();
     }
 
     public void setPower(double power){
