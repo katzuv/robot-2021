9fa6a9232c02f265a0c4305da292fdc849d17228
natymeitav <naty.meitav@Gmail.com>
2020-12-14 11:24:02 +0200

    Crate intake's Contractor.

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,15 @@
 package frc.robot.subsystems.intake;
 
-public class Intake {
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.robot.Ports;
 
+public class Intake {
+    private TalonSRX motor = new TalonSRX(Ports.Intake.MOTOR);
+    private Solenoid solenoidR = new Solenoid(Ports.Intake.SOLENOID_RIGHT);
+    private Solenoid solenoidL = new Solenoid(Ports.Intake.SOLENOID_LEFT);
+
+    public Intake() {
+        motor.setInverted(Ports.Intake.IS_INVERTED);
+    }
 }
