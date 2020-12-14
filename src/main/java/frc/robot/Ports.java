90c776ee501cf1ef8577e4f5b06fc3866030d05c
saar <saarzi2004@gmail.com>
2020-12-14 17:01:55 +0200

    Fix Ports.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,17 +3,17 @@ package frc.robot;
 public class Ports {
 
     public static class Climber {
-        public static final int MOTOR = 0;
+        public static final int MASTER = 0;
 
         public static final int SLAVE = 0;
 
         public static final int STOPPER = 0;
 
-        public static final int GEARBOX = 0;
+        public static final int GEARBOX_PISTON = 0;
 
-        public static final boolean MOTOR_INVERTED = false;
+        public static final boolean MASTER_INVERTED = false;
 
-        public static final boolean MOTOR_SENSOR_PHASE_INVERTED = false;
+        public static final boolean MASTER_SENSOR_PHASE_INVERTED = false;
 
         public static final boolean SLAVE_INVERTED = false;
 
