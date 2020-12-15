dcf6092a90b6a22e7796a95f7f40b38b1ec2450c
Barel Shlidor <barel@shlidor.org>
2020-12-15 17:52:34 +0200

    Add flywheel modules and bind them into the flywheel subsystem

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,5 +3,13 @@ package frc.robot;
 public final class Ports {
 
     public static final class Flywheel {
+        public static final int MOTOR_1 = 0;
+        public static final int MOTOR_2 = 0;
+
+        public static final boolean MOTOR_1_INVERTED = false;
+        public static final boolean MOTOR_2_INVERTED = false;
+
+        public static final boolean MOTOR_1_SENSOR_INVERTED = false;
+        public static final boolean MOTOR_2_SENSOR_INVERTED = false;
     }
 }
--- src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
+++ src/main/java/frc/robot/subsystems/flywheel/Flywheel.java
@@ -2,6 +2,16 @@ package frc.robot.subsystems.flywheel;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static frc.robot.Constants.Flywheel.*;
+import static frc.robot.Ports.Flywheel.*;
+
 public class Flywheel extends SubsystemBase {
+    private final FlywheelModule[] flywheelModules = new FlywheelModule[2]; // Maybe the quantity will change
+
+    public Flywheel() {
+        flywheelModules[0] = new FlywheelModule(MOTOR_1, MOTOR_1_INVERTED, MOTOR_1_SENSOR_INVERTED);
+        flywheelModules[1] = new FlywheelModule(MOTOR_2, MOTOR_2_INVERTED, MOTOR_2_SENSOR_INVERTED);
+
+    }
 
 }
