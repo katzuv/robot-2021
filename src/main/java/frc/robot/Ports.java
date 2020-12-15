f27d89ed2ac1b7d4a3a1f0eb902514257d86e861
natymeitav <naty.meitav@gmail.com>
2020-12-15 12:09:20 +0200

    Add Funnel's Ports.

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public class Ports {
+    public static final class Funnel{
+        public static final int MOTOR = 0;
+        public static final boolean IS_INVERTED = false;
+    }
+}
