a4e28b520a63b46f95174e259ffb88a9bf2db240
asdadsad <asdasd>
2020-12-15 14:53:06 +0200

    Build Spin & FindColour command.

--- src/main/java/frc/robot/subsystems/colour_wheel/commands/FindColour.java
+++ src/main/java/frc/robot/subsystems/colour_wheel/commands/FindColour.java
@@ -1,4 +1,71 @@
 package frc.robot.subsystems.colour_wheel.commands;
 
-public class FindColour {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.colour_wheel.ColourWheel;
+
+public class FindColour extends CommandBase {
+    private ColourWheel colourWheel;
+    private String colour;
+    private double power;
+    private String tempColour;
+    private String[] colours;
+    int finalDis;
+    int finalTemp;
+
+
+    public FindColour(ColourWheel colourWheel, String colour, double power) {
+        this.colourWheel = colourWheel;
+        this.colour = colour;
+        this.power = power;
+        colours = new String[]{"YELLOW", "BLUE", "GREEN", "RED"};
+    }
+
+    @Override
+    public void initialize() {
+        tempColour = colourWheel.getColorString();
+        colourWheel.updateSensor();
+        tempColour = colourWheel.getColorString();
+        int targetIndex = 0, currentIndex = 0;
+        for (int i = 0; i < colours.length; i++) {
+            if (colours[i].equals(colour))
+                targetIndex = i;
+            if (colours[i].equals(tempColour))
+                currentIndex = i;
+        }
+        int clockDis = 0, antiDis = 0;
+        if (targetIndex < currentIndex) {
+            clockDis = targetIndex + colours.length - currentIndex;
+            antiDis = currentIndex - targetIndex;
+        } else {
+            clockDis = targetIndex;
+            antiDis = colours.length - targetIndex + currentIndex;
+        }
+        if (clockDis < antiDis) {
+            colourWheel.setPower(power);
+            finalDis = clockDis;
+        } else {
+            colourWheel.setPower(-power);
+            finalDis = antiDis;
+        }
+    }
+
+    @Override
+    public void execute() {
+        colourWheel.updateSensor();
+        if (colourWheel.getColorString() != tempColour) {
+            tempColour = colourWheel.getColorString();
+            finalTemp--;
+        }
+        power = power - 0.1 * (finalDis - finalTemp);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        colourWheel.setPower(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return colourWheel.getColorString().equals(colour);
+    }
 }
--- src/main/java/frc/robot/subsystems/colour_wheel/commands/Spin.java
+++ src/main/java/frc/robot/subsystems/colour_wheel/commands/Spin.java
@@ -1,4 +1,52 @@
 package frc.robot.subsystems.colour_wheel.commands;
 
-public class Spin {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.colour_wheel.ColourWheel;
+
+public class Spin extends CommandBase {
+
+    private final ColourWheel colourWheel;
+    private double power;
+    private String initColour;
+    private int spinCount;
+
+    public Spin(ColourWheel colourWheel, double power) {
+        this.colourWheel = colourWheel;
+        this.power = power;
+        addRequirements(colourWheel);
+    }
+
+    @Override
+    public void initialize() {
+        colourWheel.updateSensor();
+        initColour = colourWheel.getColorString();
+        spinCount = 0;
+        colourWheel.setPower(power);
+    }
+
+    @Override
+    public void execute() {
+        colourWheel.updateSensor();
+        boolean flag = true;
+        if (!colourWheel.getColorString().equals(initColour))
+            flag = false;
+        else {
+            if (!flag) {
+                flag = true;
+                spinCount++;
+            }
+        }
+        if (spinCount == 5)
+            colourWheel.setPower(0.5 * power);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return spinCount == 6;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        colourWheel.setPower(0);
+    }
 }
