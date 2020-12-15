7e0878d7c8143989eeeca9e5d8231c3d2e94926a
natymeitav <naty.meitav@gmail.com>
2020-12-15 12:10:30 +0200

    Add Funnel Constructor.

--- src/main/java/frc/robot/subsystems/funnel/Funnel.java
+++ src/main/java/frc/robot/subsystems/funnel/Funnel.java
@@ -1,7 +1,12 @@
 package frc.robot.subsystems.funnel;
 
 import edu.wpi.first.wpilibj.Victor;
+import frc.robot.Ports;
 
 public class Funnel {
-    private Victor motor = ;
+    private Victor motor = new Victor(Ports.Funnel.MOTOR);
+
+    public Funnel(){
+        motor.setInverted(Ports.Funnel.IS_INVERTED);
+    }
 }
