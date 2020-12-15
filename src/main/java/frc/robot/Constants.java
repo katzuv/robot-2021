da4ab8f3ca5a0a736a1c09fab38c3adcb1351825
Barel Shlidor <barel@shlidor.org>
2020-12-15 11:16:00 +0200

    Add default configuration to the motor
    In the constructor I set the inversion of the motor and the sensor, config the PIDF set, set the reaction in case of neutral mode. Note: I don't need to config the sensor, because Falcons have internal encoder.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,5 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final int TALON_TIMEOUT = 10; // [ms]
 }
--- src/main/java/frc/robot/subsystems/flywheel/FlywheelModule.java
+++ src/main/java/frc/robot/subsystems/flywheel/FlywheelModule.java
@@ -1,6 +1,32 @@
 package frc.robot.subsystems.flywheel;
 
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class FlywheelModule extends SubsystemBase {
+    private final TalonFX motor;
+
+    public FlywheelModule(int port, boolean inverted, boolean sensorPhase, double[] PIDF) {
+        motor = new TalonFX(port);
+
+        motor.setInverted(inverted);
+        motor.setSensorPhase(sensorPhase);
+
+        motor.config_kP(0, PIDF[0], Constants.TALON_TIMEOUT);
+        motor.config_kI(0, PIDF[1], Constants.TALON_TIMEOUT);
+        motor.config_kD(0, PIDF[2], Constants.TALON_TIMEOUT);
+        motor.config_kF(0, PIDF[3], Constants.TALON_TIMEOUT);
+
+        motor.setNeutralMode(NeutralMode.Coast);
+
+        motor.configForwardLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.Disabled);
+        motor.configReverseLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.Disabled);
+
+        motor.enableVoltageCompensation(true);
+        motor.configVoltageCompSaturation(12);
+    }
 }
