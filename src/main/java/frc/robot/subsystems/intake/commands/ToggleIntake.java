99d756584c38a51775aff4908c2c59985c49537e
natymeitav <naty.meitav@gmail.com>
2020-12-15 11:22:47 +0200

    Fix ToggleIntake's documentation.

--- src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
+++ src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
@@ -3,12 +3,11 @@ package frc.robot.subsystems.intake.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.intake.Intake;
 
+/**
+ * this command toggles the state of intake's pistons
+ * i.e. opened --> closed & closed --> opened
+ */
 public class ToggleIntake extends InstantCommand {
-    /**
-     * this command toggles the state of intake's pistons
-     * i.e. opened --> closed & closed --> opened
-     */
-
     private Intake intake;
     public ToggleIntake(Intake i) {
         intake = i;
