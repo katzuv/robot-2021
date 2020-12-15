89d5e2d986838823cd72264ed4b4de5c0867a024
natymeitav <naty.meitav@gmail.com>
2020-12-15 13:34:26 +0200

    document Funner.

--- src/main/java/frc/robot/subsystems/funnel/Funnel.java
+++ src/main/java/frc/robot/subsystems/funnel/Funnel.java
@@ -4,13 +4,20 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
 
-public class Funnel extends SubsystemBase {
+/**
+ * this subsystem transfers balls from intake to conveyor
+ */
+public class Funnel extends SubsystemBase{
     private Victor motor = new Victor(Ports.Funnel.MOTOR);
 
     public Funnel() {
         motor.setInverted(Ports.Funnel.IS_INVERTED);
     }
 
+    /**
+     * @param velocity - the target velocity (m/s)
+     * this function sets the motor's velocity
+     */
     public void setVelocity(double velocity) {
         motor.set(velocity);
     }
