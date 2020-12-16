74d7507b07dcdfb8789992cd65b767ba26cb5c2b
f <f>
2020-12-16 11:27:58 +0200

    Change name to ShiftGear.

--- src/main/java/frc/robot/subsystems/climber/commands/ShiftGear.java
+++ src/main/java/frc/robot/subsystems/climber/commands/ShiftGear.java
@@ -3,11 +3,11 @@ package frc.robot.subsystems.climber.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.climber.Climber;
 
-public class SetGearbox extends InstantCommand {
+public class ShiftGear extends InstantCommand {
     private final Climber climber;
     private Climber.PistonMode mode;
 
-    public SetGearbox(Climber climber, Climber.PistonMode mode) {
+    public ShiftGear(Climber climber, Climber.PistonMode mode) {
         this.climber = climber;
         this.mode = mode;
         addRequirements(climber);
