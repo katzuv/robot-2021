647347fa9d5531020cdade95c3831b1cfa8e0fd4
naty <naty.meitav@Gmail.com>
2020-12-28 14:48:43 +0200

    Set motor to be VictorSPX type.

--- src/main/java/frc/robot/subsystems/funnel/Funnel.java
+++ src/main/java/frc/robot/subsystems/funnel/Funnel.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.funnel;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
@@ -8,7 +10,7 @@ import frc.robot.Ports;
  * this subsystem transfers balls from intake to conveyor
  */
 public class Funnel extends SubsystemBase {
-    private Victor motor = new Victor(Ports.Funnel.MOTOR);
+    private VictorSPX motor = new VictorSPX(Ports.Funnel.MOTOR);
 
     public Funnel() {
         motor.setInverted(Ports.Funnel.IS_INVERTED);
@@ -16,9 +18,9 @@ public class Funnel extends SubsystemBase {
 
     /**
      * this function sets the motor's velocity
-     * @param velocity - the target velocity (m/s)
+     * @param power - the target output (%)
      */
-    public void setVelocity(double velocity) {
-        motor.set(velocity);
+    public void setVelocity(double power) {
+        motor.set(ControlMode.PercentOutput , power);
     }
 }
