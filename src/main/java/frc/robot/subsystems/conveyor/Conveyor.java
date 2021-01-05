94575c90f196c1a6aa597716774dd6fb33e7d77b
Barel Shlidor <barel@shlidor.org>
2021-01-05 19:30:36 +0200

    Create function for the conveyor

--- src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/Conveyor.java
@@ -1,6 +1,13 @@
 package frc.robot.subsystems.conveyor;
 
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Ports;
+import frc.robot.subsystems.UnitModel;
+
+import static frc.robot.Constants.Conveyor.*;
 
 public class Conveyor extends SubsystemBase {
     private final TalonFX motor = new TalonFX(Ports.Conveyor.MOTOR);
@@ -8,13 +15,31 @@ public class Conveyor extends SubsystemBase {
     public Conveyor() {
         motor.setInverted(Ports.Conveyor.IS_MOTOR_INVERTED);
 
-        motor.configPeakOutputForward(Constants.Conveyor.FORWARD_PEAK, Constants.TALON_TIMEOUT);
-        motor.configPeakOutputReverse(Constants.Conveyor.REVERSE_PEAK, Constants.TALON_TIMEOUT);
+        motor.configPeakOutputForward(FORWARD_PEAK, Constants.TALON_TIMEOUT);
+        motor.configPeakOutputReverse(REVERSE_PEAK, Constants.TALON_TIMEOUT);
 
-        motor.configSupplyCurrentLimit(Constants.Conveyor.SUPPLY_CURRENT_LIMIT, Constants.TALON_TIMEOUT);
-        motor.configStatorCurrentLimit(Constants.Conveyor.STATOR_CURRENT_LIMIT, Constants.TALON_TIMEOUT);
+        motor.configSupplyCurrentLimit(SUPPLY_CURRENT_LIMIT, Constants.TALON_TIMEOUT);
+        motor.configStatorCurrentLimit(STATOR_CURRENT_LIMIT, Constants.TALON_TIMEOUT);
 
         motor.enableVoltageCompensation(true);
         motor.configVoltageCompSaturation(12);
+    }
+    public double getPosition() {
+        return unitModel.toUnits(motor.getSelectedSensorPosition());
+    }
+
+    public double getPower() {
+        return motor.getMotorOutputPercent();
+    }
+
+    public void setPower(double power) {
+        motor.set(TalonFXControlMode.PercentOutput, power);
+    }
+
+    /**
+     * Stop the motor.
+     */
+    public void stop() {
+        setPower(0);
     }
 }
