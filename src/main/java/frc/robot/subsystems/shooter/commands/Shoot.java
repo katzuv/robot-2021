ee4b3690a1bdf96cec117edac627fe142f5b1e01
Barel Shlidor <barel@shlidor.org>
2021-01-15 17:08:25 +0200

    Apply the changes to Shoot command

--- src/main/java/frc/robot/subsystems/shooter/commands/Shoot.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/Shoot.java
@@ -1,21 +1,22 @@
 package frc.robot.subsystems.shooter.commands;
 
-import java.util.function.Supplier;
-
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.shooter.Shooter;
+
+import java.util.function.Supplier;
 
 /**
  * This command keeps the shooter in the specified velocity.
  * This command should <b>NOT</b> be used in order to change the velocity applied by the shooter.
  * The command should come in conjunction with a button, and not to start/stop it after a specified time.
- * If you wish to accelerate the shooter to a specified velocity, please look at {@link SpeedUp}.
- *
- * @see SpeedUp
  */
 public class Shoot extends CommandBase {
     private final Shooter shooter;
     private final Supplier<Double> velocity;
+    private final Timer shootingTimer = new Timer();
+    private double lastTime = 0;
 
     public Shoot(Shooter shooter, Supplier<Double> velocity) {
         this.shooter = shooter;
@@ -24,8 +25,16 @@ public class Shoot extends CommandBase {
     }
 
     @Override
+    public void initialize() {
+        shootingTimer.start();
+        shooter.setVelocity(velocity.get(), Constants.LOOP_PERIOD);
+    }
+
+    @Override
     public void execute() {
-        shooter.setVelocity(velocity.get());
+        final double currentTime = shootingTimer.get();
+        shooter.setVelocity(velocity.get(), currentTime - lastTime);
+        lastTime = currentTime;
     }
 
     @Override
@@ -36,5 +45,7 @@ public class Shoot extends CommandBase {
     @Override
     public void end(boolean interrupted) {
         shooter.stop();
+        shootingTimer.stop();
+        shootingTimer.reset();
     }
 }
