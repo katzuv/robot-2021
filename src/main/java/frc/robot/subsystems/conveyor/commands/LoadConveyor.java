d91c0df83ef68256680bc380042b3b93ec86d24d
Barel Shlidor <barel@shlidor.org>
2021-01-29 11:32:27 +0200

    Add documentation to LoadConveyor command

--- src/main/java/frc/robot/subsystems/conveyor/commands/LoadConveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/LoadConveyor.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems.conveyor.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.conveyor.Conveyor;
 
+/**
+ * This command load the conveyor, as long as the conveyor isn't "out of space".
+ */
 public class LoadConveyor extends CommandBase {
     private final Conveyor conveyor;
     private final double power;
